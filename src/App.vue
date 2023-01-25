57466710fbb8156b737d841e3ffa18f197ab1c98
glmachado <glmachado@nelogica.com.br>
2023-01-25 15:46:35 -0300

    added tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="">Some say this is an app</div>
+  <div class="">
+    <p class="text-3xl mb-3">Some say this is an app</p>
+    <p class="">(who am I to saay otherwise?)</p>
+  </div>
 </template>
 
 <style scoped></style>
