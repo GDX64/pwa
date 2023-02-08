39c7d47f662ae531387c96b140f60a7ab4c590bc
glmachado <glmachado@nelogica.com.br>
2023-02-08 16:53:26 -0300

    more cards

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,7 +1,27 @@
 <template>
-    <div class="flex items-center flex-col pt-10 w-full">
-        <h1 class="text-xl">This is home</h1>
+    <div class="flex flex-col pt-10 w-full px-2">
+        <h1 class="text-xl mb-5">This is home</h1>
+        <div class="flex items-center mb-12">
+            <PossitionCard></PossitionCard>
+            <div class="text-2xl border-2 border-white w-6 h-6 rounded-full items-center flex justify-center ml-5">+</div>
+        </div>
+        <swiper-container :nested="true" slides-per-view="auto" free-mode="true" :space-between="10">
+            <swiper-slide class="w-60">
+                <PossitionCard></PossitionCard>
+            </swiper-slide>
+            <swiper-slide class="w-60">
+                <PossitionCard></PossitionCard>
+            </swiper-slide>
+            <swiper-slide class="w-60">
+                <PossitionCard></PossitionCard>
+            </swiper-slide>
+            <swiper-slide class="">
+                <PossitionCard></PossitionCard>
+            </swiper-slide>
+        </swiper-container>
     </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import PossitionCard from './PossitionCard.vue';
+</script>
---
+++ src/components/PossitionCard.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="w-60 h-32 bg-gray-800 rounded-md overflow-hidden border-l-4 border-purple-700">
+        <div class="bg-gray-700 w-full h-12 text-sm text-white font-bold pl-3 overflow-hidden flex flex-col justify-evenly">
+            <p class="whitespace-nowrap text-ellipsis overflow-hidden">Simulator - {{ accountData.owner }}</p>
+            <p class="whitespace-nowrap text-ellipsis overflow-hidden text-xs">
+                Total Balance {{ accountData.balance }} {{ accountData.currency }}
+            </p>
+        </div>
+        <div class="grid grid-cols-2 gap-1 pt-2 w-full px-2">
+            <div class="text-gray-400 text-xs col-span-2">Open PnL</div>
+            <div class="text-red-400 text-xs font-bold">-{{ accountData.open }} {{ accountData.currency }}</div>
+            <div class="text-red-400 text-xs font-bold place-self-end">-{{ accountData.openPercent }}%</div>
+            <div class="flex w-max">
+                <div class="text-gray-400 text-xs mr-1">Daily PnL</div>
+                <div class="text-green-400 text-xs font-bold place-self-end">
+                    {{ accountData.dailyResult }} {{ accountData.currency }}
+                </div>
+            </div>
+            <div class="text-green-400 text-xs font-bold place-self-end">{{ accountData.dailyPercent }}%</div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { faker } from '@faker-js/faker';
+import { reactive } from 'vue';
+
+const accountData = reactive({
+    owner: faker.name.fullName(),
+    currency: faker.finance.currencyCode(),
+    balance: faker.commerce.price(0, 1000, 2),
+    dailyResult: faker.commerce.price(0, 1000, 2),
+    open: faker.commerce.price(0, 500, 2),
+    openPercent: faker.commerce.price(0, 5, 2),
+    dailyPercent: faker.commerce.price(0, 10, 2),
+});
+</script>
